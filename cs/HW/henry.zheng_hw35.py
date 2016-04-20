bf6101740da9ab3e0256cfd4cee3ef99e5e8b293
henryz2000 <henryzheng8225@gmail.com>
2016-04-19 20:28:07 -0400

    added hw35

---
+++ cs/HW/henry.zheng_hw35.py
@@ -0,0 +1,20 @@
+#Team Idklol -- Henry Zheng, Iris Tao
+#IntroCS pd8
+#HW#35 -- Best laid plans...
+#2016-04-20
+
+'''
+Mechanism for Madlibification
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+1. create the lists of nouns/adjectives/verbs that are going to be used in the text
+   and assign a variable to each of these lists (NOUN,ADJ,etc...)
+
+2. Write a helper function that selects/returns a random element in the list.
+   This helper function will take in the list of nouns or adjs as parameter, generates a random index in the range of len(list),
+   and then returns the corresponding element in the list.
+   
+3. copy&paste the rest of the text that is given, assign a variable to each section of them.
+   (sec1,sec2,sec3,etc...)
+
+4. Write a function that takes in the sections of the text in order and adds the selected nouns/adjs in between them, and generates/returns a complete body text.
+'''
